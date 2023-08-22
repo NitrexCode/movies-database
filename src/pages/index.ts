eb9853356217c1c050d4fbad7eed77a673545078
Michael <104085580+NitrexCode@users.noreply.github.com>
2023-08-22 10:11:44 +0200

    export pages components

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as FavoritesPage } from './FavoritesPage'
+export { default as Home } from './Home'
+export { default as MovieDetailPage } from './MovieDetailPage'
+export { default as SearchPage } from './SearchPage'
