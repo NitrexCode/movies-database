2a0ac4799915dc541d2309ecb4a5588d1b09c5ce
Michael <104085580+NitrexCode@users.noreply.github.com>
2023-08-22 22:28:22 +0200

    create simple template for display favorite movies

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,10 +1,35 @@
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../redux/store'
+import { Card, CardMedia, CardContent, Typography, Grid } from '@mui/material'
 
 const FavoritesPage: React.FC = () => {
 	const favorites = useSelector((state: RootState) => state.movies.favorites)
 
-	return <div></div>
+	return (
+		<Grid container spacing={3}>
+			{favorites.map((movie) => (
+				<Grid item xs={3} key={movie.imdbID}>
+					<Card>
+						<CardMedia
+							component="img"
+							height="140"
+							image={movie.Poster}
+							alt={movie.Title}
+						/>
+						<CardContent>
+							<Typography variant="h6" component="div">
+								{movie.Title}
+							</Typography>
+							<Typography variant="body2" color="text.secondary">
+								{movie.Year}
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+			))}
+		</Grid>
+	)
 }
 
 export default FavoritesPage
