d773e8669de8e78071416d8565fe1e4b50f27e50
Michael <104085580+NitrexCode@users.noreply.github.com>
2023-08-23 00:01:00 +0200

    add redirect to detail and stop bubbing add to favorites fn

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Grid, Card, CardMedia, CardContent, Typography, IconButton } from '@mui/material'
+import { Grid, Card, CardMedia, CardContent, CardActionArea, Typography, IconButton } from '@mui/material'
 import { Star, StarBorder } from '@mui/icons-material'
+import { useNavigate } from 'react-router-dom'
 
 type MovieItem = {
     Poster: string;
     Title: string;
     Year: string;
     imdbID: string;
-};
+}
 
 type MovieGridProps = {
     movies: MovieItem[];
@@ -16,28 +17,37 @@ type MovieGridProps = {
 }
 
 const MovieGrid: React.FC<MovieGridProps> = ({ movies, toggleFavorite, isFavorite }) => {
+	
+	const navigate = useNavigate()
+
 	return (
 		<Grid container spacing={3}>
 			{movies.map((movie) => (
 				<Grid item xs={3} key={movie.imdbID}>
 					<Card>
-						<CardMedia
-							component="img"
-							height="140"
-							image={movie.Poster}
-							alt={movie.Title}
-						/>
-						<CardContent>
-							<Typography variant="h6" component="div">
-								{movie.Title}
-							</Typography>
-							<Typography variant="body2" color="text.secondary">
-								{movie.Year}
-							</Typography>
-							<IconButton onClick={() => toggleFavorite(movie)}>
-								{isFavorite(movie.imdbID) ? <Star /> : <StarBorder />}
-							</IconButton>
-						</CardContent>
+						<CardActionArea component='div' onClick={() => navigate(`/movie/${movie.imdbID}`)}>
+							<CardMedia
+								component='img'
+								height='140'
+								image={movie.Poster}
+								alt={movie.Title}
+							/>
+							<CardContent>
+								<Typography variant='h6' component='div'>
+									{movie.Title}
+								</Typography>
+								<Typography variant='body2' color='text.secondary'>
+									{movie.Year}
+								</Typography>
+								<IconButton onClick={ev => {
+									ev.stopPropagation()
+									toggleFavorite(movie)
+								}}>
+									{isFavorite(movie.imdbID) ? <Star /> : <StarBorder />}
+								</IconButton>
+							</CardContent>
+						</CardActionArea>
+
 					</Card>
 				</Grid>
 			))}
