03a23cde73028272afe3a0771b7ee5b6b26bf7af
Michael <104085580+NitrexCode@users.noreply.github.com>
2023-08-23 08:44:45 +0200

    add error and pagination handle functions

--- src/hooks/useSearchMovies.ts
+++ src/hooks/useSearchMovies.ts
@@ -1,17 +1,31 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../redux/store'
 
 const useSearchMovies = () => {
 	const [query, setQuery] = useState('')
+	const [error, setError] = useState<string | null>(null)
+	const [page, setPage] = useState(1)
 	const dispatch = useDispatch()
 	const movies = useSelector((state: RootState) => state.movies.movies)
 
 	const handleSearch = () => {
-		dispatch({ type: 'movies/fetchMovies', payload: query })
+		if (!query.trim()) {
+			setError('Please enter a search term.')
+			return
+		}
+		setError(null)
+		dispatch({ type: 'movies/fetchMovies', payload: { query, page } })
+	}
+
+	const handlePageChange = (ev: React.ChangeEvent<unknown>, value: number) => {
+		setPage(value)
+		handleSearch()
 	}
 	
-	return { query, setQuery, handleSearch, movies }
+	
+	
+	return { error, query, setQuery, handleSearch, handlePageChange, movies, page }
 }
 
 export default useSearchMovies
