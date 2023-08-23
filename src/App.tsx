c57c2274cecbeb7a33dd68e071b64fb0c4982552
Michael <104085580+NitrexCode@users.noreply.github.com>
2023-08-23 11:44:25 +0200

    added global styles for add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { FavoritesPage, Home, MovieDetailPage, SearchPage } from './pages'
+import { Global } from '@emotion/react'
+import { globalStyles } from './commonStyles'
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/search' element={<SearchPage />} />
-				<Route path='/movie/:id' element={<MovieDetailPage />} />
-				<Route path='/favorites' element={<FavoritesPage />} />
-				<Route path='/' element={<Home />} />
-			</Routes>
-		</BrowserRouter>
+		<>
+			<Global styles={globalStyles} />
+			<BrowserRouter>
+				<Routes>
+					<Route path='/search' element={<SearchPage />} />
+					<Route path='/movie/:id' element={<MovieDetailPage />} />
+					<Route path='/favorites' element={<FavoritesPage />} />
+					<Route path='/' element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</>
 	)
 }
 
