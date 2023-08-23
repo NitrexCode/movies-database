e125b3c8756d7cd6b0c9d80d029d086f8e24cfff
Michael <104085580+NitrexCode@users.noreply.github.com>
2023-08-23 08:48:26 +0200

    added page parameter for pagination

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,9 +3,9 @@ import axios from 'axios'
 const API_KEY = '8f61bcce'
 const BASE_URL = 'http://omdbapi.com/'
 
-export const searchMovies = async (query: string) => {
+export const searchMovies = async ({ query, page = 1 }: { query: string, page?: number }) => {
 	const response = await axios.get(
-		`${BASE_URL}?apikey=${API_KEY}&s=${query}`,
+		`${BASE_URL}?apikey=${API_KEY}&s=${query}&page=${page}`,
 	)
 	return response.data
 }
