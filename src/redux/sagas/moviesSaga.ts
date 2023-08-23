daa78353e3725b43147c37b8b9bd8c9bcd45334d
Michael <104085580+NitrexCode@users.noreply.github.com>
2023-08-23 08:46:43 +0200

    added page value to the payload

--- src/redux/sagas/moviesSaga.ts
+++ src/redux/sagas/moviesSaga.ts
@@ -3,9 +3,14 @@ import { PayloadAction } from '@reduxjs/toolkit'
 import { searchMovies } from '../../utils/api'
 import { setMovies } from '../moviesSlice'
 
-function* fetchMovies(action: PayloadAction<string>): Generator {
+interface FetchMoviesPayload {
+    query: string;
+    page: number;
+}
+
+function* fetchMovies(action: PayloadAction<FetchMoviesPayload>): Generator {
 	try {
-		const results = yield call(searchMovies, action.payload)
+		const results = yield call(searchMovies, { query: action.payload.query, page: action.payload.page })
 		yield put(setMovies(results))
 	} catch (error) {
 		console.error('Error fetching movies:', error)
